81ad837168391ea8ddfb404963a425469be451a1
sleekLancelot <sleekzy7@gmail.com>
2023-05-02 04:51:00 +0100

    enable cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,7 +5,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app: NestExpressApplication = await NestFactory.create(AppModule);
+  const app: NestExpressApplication = await NestFactory.create(AppModule, { cors: true });
   const config: ConfigService = app.get(ConfigService);
   const port: number = config.get<number>('PORT');
 
